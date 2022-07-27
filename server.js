70fcd63787ce24247a7792c0e61b0e9fc4c78f05
Jin Kang <kbjss071@gmail.com>
2022-07-27 16:22:36 -0400

    checked notetaker works properly

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const {v4: uuidv4} = require('uuid');
-const api = require('./public/assets/js/index');
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,7 +11,6 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
